9cf2eb83cfa23a969883f2bfe476c62071f825ea
Pierre-alban Waters <pwaters@c1r6p12.hive.fi>
2021-12-06 16:34:20 +0200

    memcmp memmove

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pwaters <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 13:05:41 by pwaters           #+#    #+#             */
-/*   Updated: 2021/12/01 11:45:29 by pwaters          ###   ########.fr       */
+/*   Updated: 2021/12/06 14:42:46 by pwaters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	str1 = (char *)s1;
 	str2 = (char *)s2;
-	while (*(str1 + i) != '\0' && *(str1 + i) == *(str2 + i) && (i < n))
+
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 		i++;
-	if (*(str1 + i) - *(str2 + i) > 0)
-		return (1);
-	else if (*(str2 + i) - *(str1 + i) > 0)
-		return (-1);
-	else
-		return (0);
+	return (0);
 }
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pwaters <pwaters@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:33:27 by pwaters           #+#    #+#             */
-/*   Updated: 2021/12/01 12:03:23 by pwaters          ###   ########.fr       */
+/*   Updated: 2021/12/06 14:31:41 by pwaters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,13 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char		*dst1;
-	const char	*src1;
-	size_t		i;
+	char	*tmp;
 
-	dst1 = dst;
-	src1 = src1;
-	i = 0;
-	if (dst == src || len == 0)
-		return (dst);
-	if (dst1 - src1 < 0)
-		while (i < len)
-		{
-			*(dst1 + i) = *(src1 + i);
-			i++;
-		}
-	else
-	{
-		i = len;
-		while (i-- > 0)
-			dst1[i] = src1[i];
-		dst1[0] = src1[0];
-	}
+	tmp = (char *)malloc(sizeof(char) * len);
+	if (tmp == NULL)
+		return (NULL);
+	ft_memcpy(tmp, src, len);
+	ft_memcpy(dst, tmp, len);
+	free(tmp);
 	return (dst);
 }
